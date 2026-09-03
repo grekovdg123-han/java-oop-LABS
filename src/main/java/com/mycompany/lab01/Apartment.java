710d4cecde73e8a3005e06510cf9df5361e62d17
Roman <4romangrekk@gmail.com>
2026-09-03 20:41:07 +0300

    Added setters with validation for Apartment class

--- src/main/java/com/mycompany/lab01/Apartment.java
+++ src/main/java/com/mycompany/lab01/Apartment.java
@@ -1,6 +1,6 @@
 public class Apartment{
     private long id;
-    private String address;
+    private String adress;
     private double area;
     private int rooms;
     private double price;
@@ -11,7 +11,7 @@ public class Apartment{
     }
     
     public String getAddress(){
-        return address;
+        return adress;
     }
     
     public double getArea(){
@@ -25,5 +25,36 @@ public class Apartment{
     public double getPrice(){
         return price;
     }
+    public void setAdress(String adress){
+        if (adress == null || adress.isEmpty()){
+            throw new IllegalArgumentException("Адрес не может быть пустым"); 
+        }
+        this.adress =adress;
+    }
+    public void setArea(double area){
+        if (area <= 0){
+            throw new IllegalArgumentException("Цена не может быть отрицательной");  
+        }
+        this.area=area;
+    }
+    public void setRooms(int rooms){
+        if(rooms < 1 || rooms >10){
+            throw new IllegalArgumentException("Комнаты не соответствуют диапозону");
+        }
+        this.rooms=rooms;
+    }
+    public void setPrice(double price){
+        if(price <= 0){
+            throw new IllegalArgumentException("Цена не может быть отрицательной");
+        }
+        this.price=price;
+    }
+    
+    public void setId(long id) {
+    if (id <= 0) {
+        throw new IllegalArgumentException("ID должен быть больше 0");
+    }
+    this.id = id;
+}
     
 }
