870478ab82ab3bd7f0f7d1c28da24032e5af7984
Roman <4romangrekk@gmail.com>
2026-09-03 14:45:55 +0300

    Initial commit: lab01 setup

---
+++ src/main/java/com/mycompany/lab01/Lab01FirstClass.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lab01;
+
+/**
+ *
+ * @author LOCAL
+ */
+public class Lab01FirstClass {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
